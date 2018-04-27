41067027d5be5129081bc0e21816df343cbaf9fc
André Magno <andremagno14@gmail.com>
2018-04-26 21:12:49 -0300

    insercao das novas activitys

--- app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/CadastroPessoalActivity.java
+++ app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/CadastroPessoalActivity.java
@@ -1,17 +1,14 @@
 package com.example.alu201513437.mobile_cadastroresumo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroPessoalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, CadastroPessoalActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_cadastro_pessoal);
     }
+
 }
--- app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/ResumoActivity.java
+++ app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/ResumoActivity.java
@@ -1,17 +1,13 @@
 package com.example.alu201513437.mobile_cadastroresumo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ResumoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, CadastroPessoalActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_resumo);
     }
 }

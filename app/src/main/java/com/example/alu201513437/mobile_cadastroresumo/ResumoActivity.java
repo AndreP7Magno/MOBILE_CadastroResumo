6d15e1e9e1113b343203244fa98dabf385a3f330
Sparta <joaoborgesc@hotmail.com>
2018-04-27 23:10:27 -0300

    Merge branch 'master' of https://github.com/AndreP7Magno/MOBILE_CadastroResumo
    # Conflicts:
    #	app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/CadastroCursoActivity.java

--- app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/ResumoActivity.java
+++ app/src/main/java/com/example/alu201513437/mobile_cadastroresumo/ResumoActivity.java
@@ -4,18 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class ResumoActivity extends AppCompatActivity {
 
-    @InjectView(R.id.txtNome) EditText txtNome;
-    @InjectView(R.id.txtLogradouroNumero) EditText txtLogradouroNumero;
-    @InjectView(R.id.txtBairroCidade) EditText txtBairroCidade;
-    @InjectView(R.id.txtTelefone) EditText txtTelefone;
-    @InjectView(R.id.txtFaculdade) EditText txtFaculdade;
-    @InjectView(R.id.txtCursoTurno) EditText txtCursoTurno;
+    @InjectView(R.id.txtNome) TextView txtNome;
+    @InjectView(R.id.txtLogradouroNumero) TextView txtLogradouroNumero;
+    @InjectView(R.id.txtBairroCidade) TextView txtBairroCidade;
+    @InjectView(R.id.txtTelefone) TextView txtTelefone;
+    @InjectView(R.id.txtFaculdade) TextView txtFaculdade;
+    @InjectView(R.id.txtCursoTurno) TextView txtCursoTurno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +26,12 @@ public class ResumoActivity extends AppCompatActivity {
 
         Intent cadastroCurso = getIntent();
 
-        txtNome.setText(cadastroCurso.getStringExtra("txtNome"));
-        txtLogradouroNumero.setText(cadastroCurso.getStringExtra("txtLogradouro") + " - " + cadastroCurso.getStringExtra("txtNumero"));
-        txtBairroCidade.setText(cadastroCurso.getStringExtra("txtBairro") + " - " + cadastroCurso.getStringExtra("txtCidade"));
-        txtTelefone.setText(cadastroCurso.getStringExtra("txtTelefone"));
-        txtFaculdade.setText(cadastroCurso.getStringExtra("txtFaculdade"));
-        txtCursoTurno.setText(cadastroCurso.getStringExtra("txtCurso") + " - " + cadastroCurso.getStringExtra("txtTurno"));
+        txtNome.setText("Nome: " + cadastroCurso.getStringExtra("txtNome"));
+        txtLogradouroNumero.setText("Logradouro/Número:" + cadastroCurso.getStringExtra("txtLogradouro") + " - " + cadastroCurso.getStringExtra("txtNumero"));
+        txtBairroCidade.setText("Bairro/Cidade" + cadastroCurso.getStringExtra("txtBairro") + " - " + cadastroCurso.getStringExtra("txtCidade"));
+        txtTelefone.setText("Telefone:" + cadastroCurso.getStringExtra("txtTelefone"));
+        txtFaculdade.setText("Faculdade:" + cadastroCurso.getStringExtra("txtFaculdade"));
+        txtCursoTurno.setText("Curso/Turno:" + cadastroCurso.getStringExtra("txtCurso") + " - " + cadastroCurso.getStringExtra("txtTurno"));
 
     }
 }
